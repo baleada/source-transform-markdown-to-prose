4319705277c237301cddb68045518223de6900ce
AlexVipond <alex.r.vipond@gmail.com>
2019-11-16 04:23:08 -0600

    new home for prose-to-markdown plugin stuff

---
+++ src/stubs/prose/index.js
@@ -0,0 +1,11 @@
+import plugins from './plugins'
+import rollup from './rollup.js'
+import transforms from './transforms.js'
+import webpack from './webpack.js'
+
+export {
+  plugins,
+  rollup,
+  transforms,
+  webpack,
+}
